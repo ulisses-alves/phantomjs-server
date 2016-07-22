c2aa5ed5ba20f3e2a48c7404d3f0f549b8903660
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2016-07-22 11:55:45 -0300

    add python startup script

---
+++ start.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from subprocess import call
+
+phantomjs = os.path.join(
+  os.environ['HOME'],
+  'site',
+  'wwwroot',
+  'bin',
+  'phantomjs.exe'
+)
+
+port = os.environ['HTTP_PLATFORM_PORT']
+
+print('Starting PhantomJS...')
+print('Using PhantomJS in: ' + phantomjs)
+print('Using port: ' + port)
+
+call(["phantomjs", "--webdriver=" + port])
