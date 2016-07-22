81ef792dc4642f96c9e5e8af0bc55a8d092c84f2
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2016-07-22 13:28:46 -0300

    add ip address

--- start.py
+++ start.py
@@ -16,4 +16,4 @@ print('Starting PhantomJS...')
 print('Using PhantomJS in: ' + phantomjs)
 print('Using port: ' + port)
 
-call([phantomjs, "--webdriver=" + port])
+call([phantomjs, "--webdriver=127.0.0.1:" + port])
